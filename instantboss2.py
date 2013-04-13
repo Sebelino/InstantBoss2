7a8ada5f512e7aa91e42955f05e3690885df25fc
Sebastian Olsson <sebelino@hotmail.com>
2013-04-13 17:17:50 +0200

    Funkar som vanligt nu.

--- instantboss2.py
+++ instantboss2.py
@@ -1,26 +1,25 @@
 import winsound,sys,time
 import os
+import thread
 
 def beep(sound):
-    winsound.PlaySound('%s.wav' % sound, winsound.SND_FILENAME)
+    winsound.PlaySound(sound,winsound.SND_FILENAME|winsound.SND_NOWAIT)
 
 def countdown(message,seconds,file):
     print(message)
-    beep(file)
+    thread.start_new_thread(beep,(file,))
     for i in range(seconds):
         os.system("title %d - %s"% (seconds-i,message))
         time.sleep(1)
 
 # The number of arguments has to be 2.
 if not len(sys.argv) == 3:
-    print("Please apply 2 parameters: the number of work minutes and the number of break"\
+    print("Please apply 2 parameters: the number of work minutes and the number of break "\
             "minutes.")
     sys.exit()
 
 work_secs = 60*int(sys.argv[1])
 break_secs = 60*int(sys.argv[2])
 while True:
-    os.startfile(r"C:\Users\Sebastian\Music\Let Me Sleep Please.mp3")
-    countdown("Get to work!",work_secs,"work")
-    os.startfile(r"C:\Users\Sebastian\Music\Hallucination.mp3")
-    countdown("Chill out.",break_secs,"break")
+    countdown("Get to work!",work_secs,"workorg")
+    countdown("Chill out.",break_secs,"breakorg")
