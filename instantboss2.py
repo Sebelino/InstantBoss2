69d23107d3708dd62797153f806f311865a40966
Sebelino <sebelino@hotmail.com>
2013-05-22 16:28:42 +0200

    Fixade så att den inte krashar efter ett tag.

--- instantboss2.py
+++ instantboss2.py
@@ -1,15 +1,17 @@
-import winsound,sys,time
-import os
+import pygame,sys,time
 import thread
 
+pygame.init()
+
 def beep(sound):
-    winsound.PlaySound(sound,winsound.SND_FILENAME|winsound.SND_NOWAIT)
+    pygame.mixer.music.load(sound)
+    pygame.mixer.music.play()
 
 def countdown(message,seconds,file):
     print(message)
     thread.start_new_thread(beep,(file,))
     for i in range(seconds):
-        os.system("title %d - %s"% (seconds-i,message))
+        print (seconds-i)
         time.sleep(1)
 
 # The number of arguments has to be 2.
@@ -21,5 +23,5 @@ if not len(sys.argv) == 3:
 work_secs = 60*int(sys.argv[1])
 break_secs = 60*int(sys.argv[2])
 while True:
-    countdown("Get to work!",work_secs,"workorg")
-    countdown("Chill out.",break_secs,"breakorg")
+    countdown("Get to work!",work_secs,"work.wav")
+    countdown("Chill out.",break_secs,"break.wav")
