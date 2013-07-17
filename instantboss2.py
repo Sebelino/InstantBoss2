4314d73d8e0bbeb1a26065b968ec64316c778fe6
Sebelino <sebelino@hotmail.com>
2013-07-17 13:27:31 +0200

    Nu borde skriptet kunna köras från en valfri working directory.

--- instantboss2.py
+++ instantboss2.py
@@ -1,5 +1,6 @@
-import pygame,sys,time
-import thread
+import pygame,sys,time,thread,os
+
+working_dir = os.path.dirname(os.path.realpath(__file__))
 
 pygame.init()
 
@@ -20,28 +21,28 @@ intervals = filter(lambda x: x.isdigit(),sys.argv[1:])
 
 if not intervals:
     print("Usage:")
-    print("python2 %s [OPTION...] [xmobar]"% sys.argv[0])
+    print("python2 %s [OPTION...] [xmobar]"% os.path.join(working_dir,sys.argv[0]))
     print("Example:")
-    print("python2 %s 25 5"% sys.argv[0])
+    print("python2 %s 25 5"% os.path.join(working_dir,sys.argv[0]))
     print("toggles between a period of 25 minutes and a period of 5 minutes.")
     sys.exit()
 
 argument_count = len(intervals)
 
 if xmobar_yes:
-    f = open("/home/sebelino/repos/InstantBoss2/dat/current_time","r+")
-    sounds = ["%s.wav"% i for i in range(1,len(intervals)+1)]
+    f = open(os.path.join(working_dir,"dat","current_time"),"r+")
+    sounds = [os.path.join(working_dir,"%s.wav"% i) for i in range(1,len(intervals)+1)]
     while True:
         for (interval_time,sound) in zip([str(60*int(m)) for m in intervals],sounds):
             beep(sound)
             f.write(interval_time)
             f.seek(0)
             for i in reversed(range(0,int(interval_time))):
-                f.write(str(i))
+                f.write("<fc=#9988FF>%s</fc>"% i)
                 f.truncate()
                 f.seek(0)
                 time.sleep(1)
 
 while True:
     for i in range(0,argument_count):
-        countdown("Initiate phase %s."% i ,60*int(intervals[i]),"%s.wav"% i)
+        countdown("Initiate phase %s."% (i+1) ,60*int(intervals[i]),"%s.wav"% (i+1))
