d41959ae002a0518ddeb165ca3f2475829e8f15c
Sebastian Olsson <sebelino@hotmail.com>
2014-06-22 22:56:44 +0200

    csvreader: --period enhanced.

--- csvreader.py
+++ csvreader.py
@@ -1,6 +1,6 @@
 #!/bin/python2
 
-import sys,time,thread,os,argparse,select,csv,datetime
+import sys,time,thread,os,argparse,select,csv,datetime,re
 
 """ :returns A processed representation of the csv file. """
 def readcsv(path):
@@ -17,7 +17,8 @@ parser.add_argument("-i","--input",type=str,metavar='file',default='schedule.csv
 parser.add_argument("-t","--topic",type=str,metavar='topic',
     help="The topic to filter on.")
 parser.add_argument("-p","--period",type=str,metavar='period',
-    help="The time period. Can be \"today\".")
+    help="The time interval. Format: \" A:B\", where the first day is today+A and the last\
+    day is today+B. By default A=-Inf, B=+Inf")
 parser.add_argument("-s","--sum",action='store_true',
     help="Sum the results.")
 args = parser.parse_args()
@@ -28,10 +29,17 @@ path = os.path.join(data_dir,args.input)
 
 intervals = readcsv(path)
 if args.period:
-    if args.period == "today":
-        intervals = [(a,t,s) for (a,t,s) in intervals if a.date() == datetime.datetime.now().date()]
-    elif args.period == "yesterday":
-        intervals = [(a,t,s) for (a,t,s) in intervals if a.date() == datetime.datetime.now().date()-datetime.timedelta(days=1)]
+    if args.period:
+        m = re.match('(-?\d*):(-?\d*)',args.period.strip())
+        if not m:
+            raise Exception("Incorrect format for argument \"period\".")
+        forever = 36500
+        first = int(m.group(1)) if m.group(1) else -forever
+        last = int(m.group(2)) if m.group(2) else forever
+        intervals = [(a,t,s) for (a,t,s) in intervals
+                if datetime.datetime.now().date()+datetime.timedelta(days=first) <= a.date()
+                and t.date() <= datetime.datetime.now().date()+datetime.timedelta(days=last)
+        ]
 intervals = [(a.ctime(),b-a,s) for (a,b,s) in intervals]
 if args.topic:
     intervals = [(a,t,s) for (a,t,s) in intervals if s == args.topic]
